27b386247a35acd59b188e02f65e046832d3c209
OnedariDaisakusen <65679065+OnedariDaisakusen@users.noreply.github.com>
2023-11-04 21:44:57 -0500

    se agrego codigo para ejecucion en segundo plano

--- scrapingOsiptel.py
+++ scrapingOsiptel.py
@@ -3,11 +3,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless=old")
 
-driver = webdriver.Chrome(options=options)
 #driver = webdriver.Chrome()
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument(f'user-agent={user_agent}')
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--allow-running-insecure-content')
+options.add_argument("--disable-extensions")
+options.add_argument("--proxy-server='direct://'")
+options.add_argument("--proxy-bypass-list=*")
+options.add_argument("--start-maximized")
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(options=options)
 
 driver.get('https://checatuslineas.osiptel.gob.pe/')
 
